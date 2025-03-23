fd49da1581e53b220867a328e548fd2bfd32b6f3
Daniel Kelly <me@dankelly.dev>
2025-03-23 10:39:24 -0400

    Initial commit
    Adds the tap with questie installation

---
+++ Casks/questie.rb
@@ -0,0 +1,21 @@
+cask "questie" do
+  version "10.19.0"
+  sha256 "02a4d9b42e4a11058d20f49839fec46e3fbba3310635a69a859612cd3c7f498c"
+
+  url "https://github.com/Questie/Questie/releases/download/v#{version}/Questie-v#{version}.zip"
+  name "Questie"
+  desc "Questie: The WoW Classic quest helper"
+  homepage "https://github.com/Questie/Questie"
+
+  addon_dir = File.expand_path("~/Applications/World of Warcraft/_classic_era_/Interface/AddOns")
+
+  preflight do
+    FileUtils.mkdir_p addon_dir unless Dir.exist?(addon_dir)
+  end
+
+  artifact "Questie", target: "#{addon_dir}/Questie"
+
+  zap trash: [
+    "#{addon_dir}/Questie"
+  ]
+end
---
+++ README.md
@@ -0,0 +1,23 @@
+# WoW Classic Era Addons Homebrew Tap
+
+This is a [third-party Homebrew tap](https://docs.brew.sh/Taps) that allows users to install classic-era WoW addons via [`brew`](https://brew.sh) on macOS.
+
+## Usage
+
+Add the tap.
+
+```bash
+brew tap dbk91/wow-classic-era-addons
+```
+
+Install addons with the fully-qualified name.
+
+```bash
+brew install --cask dbk91/tap/wow-classic-era-addons/<addon-name>
+```
+
+## Available Addons
+
+```
+brew install dbk91/tap/wow-classic-era-addons/questie
+```
